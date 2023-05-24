7d449b44b93dc0b3c87e21070bf351ca693038aa
Alex <l3xpr1v@proton.me>
2023-05-23 20:28:41 -0500

    added bugs - fixed table

--- binaryconv/main.py
+++ binaryconv/main.py
@@ -8,6 +8,8 @@ from tabulate import tabulate
 
 #create a random number 0-255
 def Main():
+    
+    
     def createRandomBinary():
         randomBinary = ''
         for i in range(8):
@@ -22,6 +24,31 @@ def Main():
         return randomBinary
         #returns int
         #DONE
+        
+        
+    #create function to make an 8 digit binary given the translate decimal function
+    def fixBinary(givenNum):
+        if len(givenNum) < 8: 
+            difference = 8 - len(givenNum)
+            for i in range(difference):
+                givenNum = ''.join(('0',givenNum))
+            return(givenNum)
+        if len(givenNum) > 8:
+            givenNum = givenNum[-8:]
+            return(givenNum)
+        #returns String
+        #done
+
+    #make sure to take a string in the argument
+    def binaryToDecimal(binaryNum):
+        x = 0
+        #binaryNum = binaryNum[::-1]
+        start = 7
+        for i in range(len(binaryNum)):
+            x += (2 ** start) * int(binaryNum[i])
+            start -= 1
+        return x
+    
     
     def translateDecimal(decimal):
         binary = ''
@@ -29,27 +56,71 @@ def Main():
             binary = (decimal // 2)
         binary += str(decimal%2)
         return binary
+        #fix variable names
         #returns String        
         #DONE
-            
+         
+    ################################################################
+    #try to create a try function
+    def tryBinaryToDecimal(currentBinary, userInput, lives):
+        correctAnswer = binaryToDecimal(currentBinary)
+        if currentBinary == userInput:
+            print("correct do you want to try again")
+        else:
+            print("not quite right")       
+            lives -= 1
+             
+        
+
+    
+    ################################################################
+    #try to create a try function
+    #def tryDecimalToBinary()     
+
+    
+    
+    ################################################################
     
     options = ['first', 'f', 'second', 's']
-    print(text.welcomeScreen[0])
-    table = [["Do you want to convert binary to decimal?", "Enter 'first' or 'f'"], ["Or do you want to convert Decimal to Binary?", "Enter 'second' or 's'"]]
-    print(tabulate(table))
+    
+    tableRules = [["Do you want to convert binary to decimal?", "Enter 'first' or 'f'"], ["Or do you want to convert Decimal to Binary?", "Enter 'second' or 's'"]]
+    
+    information = [
+        ["1", "You have 2 lives"],
+        ["2", "Help is only available in the main menu"],
+        ["3", "All decimal values are | 0 < x < 255"],
+        ["4", "All binary values | 0 < x < 11111111"]
+        ]
+
+    formatted_table = tabulate(information, headers=["", "Information"], tablefmt="fancy_grid")
+    items = [text.welcomeScreen[0],formatted_table]
+    formatted_table = tabulate([items], tablefmt="fancy_grid")
+    
+    print(formatted_table)
+    print(tabulate(tableRules))
     print("To exit press 'e'\n")
     
+    ################################################################
+    
     def playGame(startOption):
+        lives = 2
+        def printLives():
+            print("lives " + str(lives))
+        
         if startOption in ["first" ,'f']:
-            print("the given binary = " + str(createRandomBinary()))
+            newGameBinaryNum = createRandomBinary()
+            printLives()
+            print("the given binary = " + str(newGameBinaryNum))
+            userAttempt = input("guess??")
             
         else:
+            printLives()
             print("the given decimal = " + str(createRandomDecimal()))
             
         exit()
     
     while True:
-        init = input("enter your choice \n")
+        init = input("enter your choice    ")
         if init in options:
             playGame(init)
         if init == 'e':
--- binaryconv/testing.py
+++ binaryconv/testing.py
@@ -21,6 +21,27 @@ def fixBinary(givenNum):
     if len(givenNum) > 8:
         givenNum = givenNum[-8:]
         return(givenNum)
+
+
+#make sure to take a string in the argument
+def binaryToDecimal(binaryNum):
+    x = 0
+    #binaryNum = binaryNum[::-1]
+    start = 7
+    for i in range(len(binaryNum)):
+        x += (2 ** start) * int(binaryNum[i])
+        start -= 1
+    return x
+        
+xx = binaryToDecimal('10110101')
+print(xx)
+
+
+num = 0
+if num not in range(256):
+    print("this doesnt work")
     
-result = DecimalToBinary(255)
-print(fixBinary(result))
+x = '123'
+x = int(x)
+print(x)
+print(type(x))
